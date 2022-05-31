1ee7469b789ca40f166ebdce19be2de9918d2409
Imanol <s17120241@alumnos.itsur.edu.mx>
2022-05-30 21:50:59 -0500

    Realizar una aplicación en Android que permita responder de manera automática con un SMS, cuando entre la llamada de un número especificado por el usuario a través de la aplicación. El texto de la respuesta automática se ingresa en la aplicación, así como el número al que se le responderá automáticamente.

---
+++ app/src/main/java/net/ivanvega/mireceiverb/MainActivity.java
@@ -0,0 +1,170 @@
+package net.ivanvega.mireceiverb;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.PhoneStateListener;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import net.ivanvega.mireceiverb.receiver.MiBroadcastReceiverB;
+
+public class MainActivity extends AppCompatActivity {
+    public String txtNumero,NumLlamada,txtMensaje;
+
+    public EditText number, message;
+    public Button send;
+
+    final int SEND_SMS_PERMISSION_REQUEST_CODE = 111;
+    private  Button mSendMessageBtn;
+    private TelephonyManager mTelephonyManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mDialButton = (Button) findViewById(R.id.btnProMensaje);
+        final EditText mPhoneNoEt = (EditText) findViewById(R.id.txtNum);
+        number = findViewById(R.id.txtNum);
+        message = findViewById(R.id.txtMen);
+        send=findViewById(R.id.btnMenDif);
+
+        mDialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNo = mPhoneNoEt.getText().toString();
+                if(!TextUtils.isEmpty(phoneNo)) {
+                    String dial = "tel:" + phoneNo;
+                    startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+                }else {
+                    Toast.makeText(MainActivity.this, "Enter a phone number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        Button sendMessageBtn = (Button) findViewById(R.id.btnMenDif);
+        final EditText messagetEt = (EditText) findViewById(R.id.txtMen);
+        sendMessageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = messagetEt.getText().toString();
+                String phoneNo = mPhoneNoEt.getText().toString();
+                if(!TextUtils.isEmpty(message) && !TextUtils.isEmpty(phoneNo)) {
+                    Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + phoneNo));
+                    smsIntent.putExtra("sms_body", message);
+                    startActivity(smsIntent);
+                }
+            }
+        });
+        mSendMessageBtn = (Button) findViewById(R.id.btnMenDif);
+
+        mSendMessageBtn.setEnabled(false);
+        if(checkPermission(Manifest.permission.SEND_SMS)) {
+            mSendMessageBtn.setEnabled(true);
+        }else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS},
+                    SEND_SMS_PERMISSION_REQUEST_CODE);
+        }
+        mSendMessageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String message = messagetEt.getText().toString();
+                //String phoneNo = mPhoneNoEt.getText().toString();
+                // if(!TextUtils.isEmpty(message) && !TextUtils.isEmpty(phoneNo)) {
+
+                // if(checkPermission(Manifest.permission.SEND_SMS)) {
+                // SmsManager smsManager = SmsManager.getDefault();
+                //  smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                //}else {
+                //    Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                //  }
+                //}
+                send();
+            }
+        });
+
+
+        // ...
+        mTelephonyManager = (TelephonyManager) getSystemService(getApplicationContext().TELEPHONY_SERVICE);
+
+    }
+    private boolean checkPermission(String permission) {
+        int checkPermission = ContextCompat.checkSelfPermission(this, permission);
+        return (checkPermission == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case SEND_SMS_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    mSendMessageBtn.setEnabled(true);
+                }
+                return;
+            }
+        }
+    }
+    PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+
+            switch (state) {
+                case TelephonyManager.CALL_STATE_IDLE:
+                    Toast.makeText(MainActivity.this, "CALL_STATE_IDLE", Toast.LENGTH_SHORT).show();
+                    break;
+                case TelephonyManager.CALL_STATE_RINGING:
+                    Toast.makeText(MainActivity.this, "CALL_STATE_RINGING", Toast.LENGTH_SHORT).show();
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    Toast.makeText(MainActivity.this, "CALL_STATE_OFFHOOK", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+    }
+    public void send(){
+        txtNumero = number.getText().toString();
+        txtMensaje = message.getText().toString();
+
+        String phoneNumber = this.txtNumero;
+        String smsMessage = this.txtMensaje;
+        if(checkPermission(Manifest.permission.SEND_SMS)){
+            if(phoneNumber.equals(new MiBroadcastReceiverB().outgoingPhoneNo)){
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNumber,null,smsMessage,null,null);
+                Toast.makeText(this,"Mensaje enviado..",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"Mensaje No enviado..",Toast.LENGTH_LONG).show();
+
+            }
+        }else{
+            Toast.makeText(this,"Permiso negado",Toast.LENGTH_LONG).show();
+
+        }
+    }
+}
